0022e76a52ec539bc42e440358dd7242d467e137
Santoo-san <67684816+Santoo-san@users.noreply.github.com>
2020-09-11 16:39:45 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,115 @@
+console.log("colorgame");
+
+
+const dropdownRemove = function () {
+  const dropdownRemoval = document.getElementById("dropdown-content");
+  console.log(dropdownRemoval);
+  dropdownRemoval.addEventListener("click", function () {
+    dropdownRemoval.classList.add("remove");
+  });
+};
+
+//coloring
+//white
+
+const addWhite = function () {
+  const bodyElement = document.body;
+  bodyElement.classList = "white-background";
+};
+
+const attachWhite = function () {
+  const makeWhite = document.getElementById("white");
+  makeWhite.addEventListener("click", function () {
+    addWhite();
+  });
+};
+
+//red
+
+const addRed = function () {
+  const bodyElement = document.body;
+  bodyElement.classList = "red-background";
+};
+
+const attachRed = function () {
+  const makeRed = document.getElementById("red");
+  makeRed.addEventListener("click", function () {
+    addRed();
+  });
+};
+
+//orange
+const addOrange = function () {
+  const bodyElement = document.body;
+  bodyElement.classList = "orange-background";
+};
+
+const attachOrange = function () {
+  const makeOrange = document.getElementById("orange");
+  makeOrange.addEventListener("click", function () {
+    addOrange();
+  });
+};
+
+//yellow
+const addYellow = function () {
+  const bodyElement = document.body;
+  bodyElement.classList = "yellow-background";
+};
+
+const attachYellow = function () {
+  const makeYellow = document.getElementById("yellow");
+  makeYellow.addEventListener("click", function () {
+    addYellow();
+  });
+};
+
+//green
+const addGreen = function () {
+  const bodyElement = document.body;
+  bodyElement.classList = "green-background";
+};
+
+const attachGreen = function () {
+  const makeGreen = document.getElementById("green");
+  makeGreen.addEventListener("click", function () {
+    addGreen();
+  });
+};
+
+//blue
+const addBlue = function () {
+  const bodyElement = document.body;
+  bodyElement.classList = "blue-background";
+};
+
+const attachBlue = function () {
+  const makeBlue = document.getElementById("blue");
+  makeBlue.addEventListener("click", function () {
+    addBlue();
+  });
+};
+
+//purple
+const addPurple = function () {
+  const bodyElement = document.body;
+  bodyElement.classList = "purple-background";
+};
+
+const attachPurple = function () {
+  const makePurple = document.getElementById("purple");
+  makePurple.addEventListener("click", function () {
+    addPurple();
+  });
+};
+
+// function calls
+
+dropdownRemove();
+attachWhite();
+attachRed();
+attachOrange();
+attachYellow();
+attachGreen();
+attachBlue();
+attachPurple();
